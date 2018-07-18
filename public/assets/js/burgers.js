81a6bbf06733f90ec0905cac893ee1fda61989fb
Luz_Hernandez <hernandezluz18@gmail.com>
2018-07-17 23:45:57 -0400

    added files

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,38 @@
+$(function() {
+  $(".change-devoured").on("click", function(event) {
+    var id = $(this).data("id");
+    var newDevoured = $(this).data("newdevoured");
+
+    var newDevouredState = {
+      devoured: newDevoured
+    };
+
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevouredState
+    }).then(
+      function() {
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    event.preventDefault();
+
+    var newBurger = {
+      name: $("#burger").val().trim(),
+      devoured: false
+    };
+
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        location.reload();
+      }
+    );
+  });
+});
